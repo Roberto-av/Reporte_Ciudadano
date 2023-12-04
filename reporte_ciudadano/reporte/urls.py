99db04ce1948fe554b9d5433a52136e2a788a77c
Roberto Aviles <avilesrobertoe@gmail.com>
2023-12-04 02:17:16 -0700

    Logout

--- reporte_ciudadano/reporte/urls.py
+++ reporte_ciudadano/reporte/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-     path('', views.inicio, name='inicio'),
+     path('inicio', views.inicio, name='index'),
      path('login/', views.login, name='login'),
      path('registro/', views.registro, name='registro'),
-    # path('/reportes', views.reportes, name='reportes'),
+     path('reporte', views.reporte, name='reporte'),
+     path('logout', views.logout, name='logout'),
 ]
--- reporte_ciudadano/reporte/views.py
+++ reporte_ciudadano/reporte/views.py
@@ -5,10 +5,17 @@ from django.db import IntegrityError
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import authenticate, login
 from django.contrib import messages
+from django.contrib.auth import logout as django_logout
 
 # Create your views here.
 def inicio(request):
-    return render(request,'base.html')
+    return render(request,'index.html')
+
+def reporte(request):
+    if 'usuario_id' not in request.session:
+        # Redirigir a la página de inicio de sesión
+        return redirect('login')
+    return render(request,'reporte.html')
 
 def login(request):
     if request.method == 'POST':
@@ -25,12 +32,23 @@ def login(request):
             # Autenticación exitosa, puedes hacer lo que necesites aquí, como establecer una sesión
             # Por ejemplo, podrías usar sesiones de Django para mantener al usuario autenticado
             request.session['usuario_id'] = usuario.id_usuario
-            return redirect('registro')
+            return redirect('index')
         else:
             # Si el usuario no existe o las credenciales son incorrectas, muestra un mensaje de error
             messages.error(request, 'El correo o la contraseña son incorrectos, Intentelo nuevamente.')
 
     return render(request, 'login.html')
+
+def logout(request):
+     # Cerrar sesión de Django
+    django_logout(request)
+    
+    # Eliminar cualquier otra información de sesión que hayas creado
+    if 'usuario_id' in request.session:
+        del request.session['usuario_id']
+        
+    return redirect('index')
+
 
 def registro(request):
     if request.method == 'GET':
